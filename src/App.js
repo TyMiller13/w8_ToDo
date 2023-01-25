785cbb4e05dec3f12153cf8543a4a48830299759
unknown <tylermiller1321@outlook.com>
2023-01-24 22:04:10 -0600

    fetched student API and implemented data of each student to table

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import ToDo from "./components/ToDo";
 // import ToDoCard from "./components/ToDoCard";
+import StudentDisplay from "./components/StudentDisplay";
+
 
 function App() {
+
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        console.log('useEffect effect callback has been called');
+        fetch("https://scratched-juniper-salto.glitch.me/kekambas")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                const studentList = data
+                setStudents(studentList);
+            })
+    })
+function updateStudent(students){
+    setStudents(students)
+}
+
+
+
     return (
         <>
             <Navbar/>
             <div className="container text-center mt-4">
             <h1 className='text-center fw-bold'> Your Ultimate To-Do List</h1>
             <ToDo/>
+            <StudentDisplay students={students} updateStudent={updateStudent}/>
             </div>
             
         </>
--- src/components/StudentDisplay.jsx
+++ src/components/StudentDisplay.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
 
-export default function StudentDisplay() {
+export default function StudentDisplay(props) {
+
+    const tableHeaders = ['ID#', 'First Name', 'Last Name'];
+
+
   return (
-    <div>StudentDisplay</div>
+    <div>
+        <h1 className='text-center my-3'> Kekambas Students </h1>
+
+        <table className="table table-dark table-hover">
+            <thead>
+                <tr>
+                    {tableHeaders.map((headerName, i) => <th key={i} >{headerName}</th>)}
+                </tr>
+            </thead>
+            <tbody>
+                {props.students.map(student => (
+                    <tr key={student.id}>
+                        <th>{student.id}</th>
+                        <th>{student.first_name}</th>
+                        <th>{student.last_name}</th>  
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
   )
 }
